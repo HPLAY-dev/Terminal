d33fc079f3fa4c2304d9931b55d43254b0d3902c
VMTeam <75873267+HPLAY-dev@users.noreply.github.com>
2022-06-15 10:17:54 +0800

    1.1.2

--- terminal.py
+++ terminal.py
@@ -5,19 +5,18 @@ from datetime import datetime
 import calendar
 
 
+size = os.get_terminal_size()
 weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 day = datetime.today().day
 weekday = datetime.today().weekday()
 year = datetime.today().year
 month = datetime.today().month
-if len(str(month)) == 1:
-    smonth = '0' + str(month)
+smonth = months[month]
 
 
 def cmd_clear():
     os.system('cls')
-    os.system('title Terminal - bash - 80x24')
 def help():
     with open('help.txt', encoding='utf-8') as file_obj:
         contents = file_obj.read()
@@ -37,15 +36,18 @@ cmd_clear()
 print('Last login: ' + weekdays[weekday] + ' ' + months[month] + ' ' + str(day) + ' ' + time.strftime('%H:%M:%S') + ' on console')
 print('Welcome to Darwin！')
 while True:
+    os.system(f'title Terminal - bash - {size.columns}x{size.lines}')
     run = input('VMTD:~ VMTD$ ')
-    if run == 'clear':
-        cmd_clear()
-    elif run == 'exit':
-        cmd_exit()
-    elif run == 'help':
-        help()
-    elif run == 'cal':
-        cal()
-    else:
-        print(f'-bash: {run}: command not found')
-        
+    if run != '':
+        if run == 'clear':
+            cmd_clear()
+        if run[: 4] == 'echo':
+            print(run[5 :])
+        elif run == 'exit':
+            cmd_exit()
+        elif run == 'help':
+            help()
+        elif run == 'cal':
+            cal()
+        else:
+            print(f'-bash: {run}: command not found')
