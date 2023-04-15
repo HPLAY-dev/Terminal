232936689385af7131ca07f824d1c7f763e3e924
VMTeam <75873267+HPLAY-dev@users.noreply.github.com>
2023-04-15 16:09:46 +0800

    Add files via upload

--- terminal.py
+++ terminal.py
@@ -13,6 +13,9 @@ weekday = datetime.today().weekday()
 year = datetime.today().year
 month = datetime.today().month
 smonth = months[month]
+cdir = "C:/"
+cdir_recover = ""
+fix = ''
 
 
 def cmd_clear():
@@ -38,10 +41,41 @@ print('Welcome to Darwin！')
 while True:
     os.system(f'title Terminal - bash - {size.columns}x{size.lines}')
     run = input('VMTD:~ VMTD$ ')
-    if run != '':
-        if run == 'clear':
+    if run.isspace() == False and run != '':            # 判断是否为空
+        if run == 'clear':                              # clear
             cmd_clear()
-        if run[: 4] == 'echo':
+        elif run == 'pwd':
+            print(cdir)
+        elif run[:2].lower() == 'cd':                   # cd
+            cdir_recover = cdir
+            if run[4:6] == ':/':
+                cdir = run
+            elif run[4:5] == ':':
+                continue
+            else:
+                if run[3:] == '..' or run[3:] == '../':
+                    print(cdir)
+                    cdir = list(cdir)
+                    if cdir[-1] != '/':
+                        for i in range(len(cdir) - cdir[::-1].index('/') + 1):
+                            cdir.pop()
+                        cdir = ''.join(cdir)
+                    else:
+                        cdir.pop()
+                        for i in range(len(cdir) - cdir[::-1].index('/') + 1):
+                            cdir.pop()
+                        cdir = ''.join(cdir)
+                    for i in range(len(cdir) - 1):
+                        fix = fix + cdir[i]
+                    cdir = fix
+                elif cdir[-1] == '/':
+                    cdir = cdir + run[3:]
+                else:
+                    cdir = cdir + '/' + run[3:]
+                if os.path.exists(cdir) == False:
+                    print(cdir + ": no such directory")
+                    cdir = cdir_recover
+        elif run[:4].lower() == 'echo':                 # echo
             print(run[5 :])
         elif run == 'exit':
             cmd_exit()
